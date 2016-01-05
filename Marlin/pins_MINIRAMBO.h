2c61c4f3178f5ea47376f85644ca35c2bf84a72e
Jason Rhubottom <jason@rhusoft.com>
2016-01-05 14:49:51 -0800

    Mini Rambo configuration

--- Marlin/pins_MINIRAMBO.h
+++ Marlin/pins_MINIRAMBO.h
@@ -67,30 +67,30 @@
 #define E0_MS1_PIN          65
 #define E0_MS2_PIN          66
 
-#define E1_STEP_PIN         -1
-#define E1_DIR_PIN          -1
-#define E1_ENABLE_PIN       -1
-#define E1_MS1_PIN          -1
-#define E1_MS2_PIN          -1
+#define E1_STEP_PIN         -33
+#define E1_DIR_PIN          -42
+#define E1_ENABLE_PIN       -25
+#define E1_MS1_PIN          -63
+#define E1_MS2_PIN          -64
 
-#define MOTOR_CURRENT_PWM_XY_PIN 46
+#define MOTOR_CURRENT_PWM_XY_PIN 44
 #define MOTOR_CURRENT_PWM_Z_PIN  45
-#define MOTOR_CURRENT_PWM_E_PIN  44
+#define MOTOR_CURRENT_PWM_E_PIN  46
 //Motor current PWM conversion, PWM value = MotorCurrentSetting * 255 / range
 #define MOTOR_CURRENT_PWM_RANGE 2000
-#define DEFAULT_PWM_MOTOR_CURRENT  {1300, 1300, 1250}
+#define DEFAULT_PWM_MOTOR_CURRENT  {1300, 1630, 1250}
 #define SDPOWER             -1
 #define SDSS                53
 #define LED_PIN             13
-#define FAN_PIN              8
-#define FAN_1_PIN            6
-#define PS_ON_PIN           -1
+#define FAN_PIN            8  //Fan0 --- Extruder0 Fan
+#define FAN1_PIN           -1  //Fan1 --- Extruder1 Fan
+#define FAN3_PIN           6  //Fan3 --- Case Fan
 #define KILL_PIN            -1  // 80 with Smart Controller LCD
 #define SUICIDE_PIN         -1  // PIN that has to be turned on right after start, to keep power flowing.
 
 #if ENABLED(ULTRA_LCD)
 
-  #define KILL_PIN          32
+  #define KILL_PIN          -1
 
   #if ENABLED(NEWPANEL)
 
