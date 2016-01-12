43a167db2fde4c62a4ffaaa7f37513877afadbd6
jrhubott <jason@rhusoft.com>
2016-01-11 22:51:21 -0800

    Fixed output file name

--- .travis_deploy.sh
+++ .travis_deploy.sh
@@ -2,6 +2,6 @@
 
 if [ "$TRAVIS" ]; then #running under travis
 	if [ "$TRAVIS_TAG" ]; then #tag build in 
-	cp build/Marlin.cpp.hex "build/Marlin-$TRAVIS_TAG.hex"
+	cp build/Marlin.cpp.hex "build/MarlinMini-$TRAVIS_TAG.hex"
 	fi
 fi
