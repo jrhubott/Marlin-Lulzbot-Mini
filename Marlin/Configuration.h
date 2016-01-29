83f588b29ccdb4b33573a1ac589c4f1e1b0a0dec
Jason Rhubottom <jason@rhusoft.com>
2016-01-28 20:10:23 -0800

    Re-enabled endstops

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -398,7 +398,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define Z_HOME_DIR 1
 
 #define min_software_endstops false // If true, axis won't move to coordinates less than HOME_POS.
-#define max_software_endstops false  // If true, axis won't move to coordinates greater than the defined lengths below.
+#define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 // @section machine
 
